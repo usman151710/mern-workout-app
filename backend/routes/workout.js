1c3eefeaf48e138f4c97adbcfbbf8fae5daf0b8a
Mohammad Usman <74015597+usman151710@users.noreply.github.com>
2023-05-24 16:02:39 +0500

    Revert "create controllers for getAllWorkouts, getWorkoutbyID and createWorkout"

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const Workout = require('../models/workout');
-const { createWorkout, getAllWorkouts, getWorkoutByID } = require('../controllers/workoutController');
 
 const router = express.Router();
 
-router.get('/', getAllWorkouts);
+router.get('/', (req, res) => {
+    res.json({ msg: "Get all workouts" })
+});
 
-router.get('/:id', getWorkoutByID);
+router.get('/:id', (req, res) => {
+    res.json({ msg: "Get single workout" })
+});
 
-router.post('/', createWorkout);
+router.post('/', async (req, res) => {
+    const { title, load, reps } = req.body;
+    try {
+        const workout = await Workout.create({ title, load, reps });
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 router.patch('/:id', (req, res) => {
     res.json({ msg: "update a workout" })
