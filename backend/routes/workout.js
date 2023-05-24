7825944b15b443864de58801de12e327420d0ef8
m.usman <m.usman@medznmore.com>
2023-05-24 15:25:16 +0500

    create workout schema

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,25 +1,32 @@
 const express = require('express');
+const Workout = require('../models/workout');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({msg: "Get all workouts"})
+    res.json({ msg: "Get all workouts" })
 });
 
 router.get('/:id', (req, res) => {
-    res.json({msg: "Get single workout"})
+    res.json({ msg: "Get single workout" })
 });
 
-router.post('/', (req, res) => {
-    res.json({msg: "post new workout"})
+router.post('/', async (req, res) => {
+    const { title, load, reps } = req.body;
+    try {
+        const workout = await Workout.create({ title, load, reps });
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 router.patch('/:id', (req, res) => {
-    res.json({msg: "update a workout"})
+    res.json({ msg: "update a workout" })
 });
 
 router.delete('/:id', (req, res) => {
-    res.json({msg: "Delete single workout"})
+    res.json({ msg: "Delete single workout" })
 });
 
 module.exports = router;
