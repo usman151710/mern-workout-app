4ccbea8fba7b9f10544d16132d81212a9a8f63fb
m.usman <m.usman@medznmore.com>
2023-05-24 11:06:15 +0500

    connect app with mongo DB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 const workoutRoutes = require('./routes/workout');
 
@@ -8,7 +9,13 @@ app.listen(process.env.PORT);
 
 app.use(express.json());
 
-app.use('/api/workouts', workoutRoutes)
+app.use('/api/workouts', workoutRoutes);
+
+mongoose.connect(process.env.DB_URI)
+    .then((result) => {
+    })
+    .catch((err) => {
+    })
 
 app.get('/', (req, res) => {
     res.json({ msg: "Welecome to the app" })
