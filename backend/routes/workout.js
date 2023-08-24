efe173dd21916f3a5d28ff2f584a9b8ff6090a51
Mohammad Usman <muhammad.usman1511710@gmail.com>
2023-08-24 19:34:47 +0500

    add authentication middleware on workout routes

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const { createWorkout, getAllWorkouts, getWorkoutByID, deleteWorkout, updateWorkout } = require('../controllers/workoutController');
+const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
+
+//require authentication for all routes
+router.use(requireAuth);
 
 router.get('/', getAllWorkouts);
 
